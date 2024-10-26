28f876140c3c518b701e3f106fcd8b21cfec7fb6
se-iram <eramhameed5@gmail.com>
2024-10-26 12:14:05 +0500

    javascript

--- 2. JavaScript Basics by Role Playing Game/script.js
+++ 2. JavaScript Basics by Role Playing Game/script.js
@@ -33,6 +33,25 @@ const weapons = [
         power: 100
     }
 ]
+const monsters = [
+    {
+        name: "slime",
+        level: 2,
+        health: 15
+    },
+    {
+        name: "fanged beast",
+        level: 8,
+        health: 60
+    },
+    {
+        name: "dragon",
+        level: 20,
+        health: 300
+    },
+]
+
+
 const locations = [
     {
         name: "town square",
@@ -51,22 +70,48 @@ const locations = [
         "button text": ["Fight slime", "Fight fanged beast", "Go to town square"],
         "button functions": [fightSlime, fightBeast, goTown],
         text: "You enter the cave. You see some monsters."
+    },
+    {
+        name: "fight",
+        "button text": ["Attack", "Dodge", "Run"],
+        "button functions": [attack, dodge, goTown],
+        text: "You are fighting a monster."
+    },
+    {
+        name: "kill monster",
+        "button text": ["Go to town square", "Go to town square", "Go to town square"],
+        "button functions": [goTown, goTown, goTown],
+        text: 'The monster screams "Arg!" as it dies. You gain experience points and find gold.'
+    },
+    {
+        name: "lose",
+        "button text": ["REPLAY?", "REPLAY?", "REPLAY?"],
+        "button functions": [restart, restart, restart],
+        text: 'You die. &#x2620;'
+    },
+    {
+        name: "win",
+        "button text": ["REPLAY?", "REPLAY?", "REPLAY?"],
+        "button functions": [restart, restart, restart],
+        text: "You defeat the dragon! YOU WIN THE GAME! &#x1F389;"
     }
+
   ];
 
 // initialize buttons
 button1.onclick = goStore
 button2.onclick = goCave
-//button3.onclick = fightDragon
+button3.onclick = fightDragon
 
 function update(location) {
+    monsterStats.style.display = "none"
     button1.innerText = location["button text"][0];
     button2.innerText = location["button text"][1];
     button3.innerText = location["button text"][2];
     button1.onclick = location["button functions"][0];
     button2.onclick = location["button functions"][1];
     button3.onclick = location["button functions"][2];
-    text.innerText = location.text;
+    text.innerHTML = location.text;
 }
 
 
@@ -123,13 +168,68 @@ function sellWeapon(){
     }
 }
 
-function fightDragon(){
-
-}
 function fightSlime(){
-
+    fighting = 0
+    goFight()
 }
 function fightBeast(){
-    //
-    
+    fighting = 1
+    goFight()   
+}
+function fightDragon(){
+    fighting = 2
+    goFight()
+}
+
+function goFight(){
+    update(locations[3])
+    monsterHealth = monsters[fighting].health
+    monsterStats.style.display = 'block'
+    monsterName.innerText = monsters[fighting].name
+    monsterHealthText.innerText = monsterHealth
+}
+function attack(){
+    text.innerText = "The " + monsters[fighting].name + " attacks.";
+    text.innerText += " You attack it with your " + weapons[currentWeaponIndex].name + "."
+    health -= monsters[fighting].level;
+    monsterHealth -= weapons[currentWeaponIndex].power + Math.floor(Math.random() * xp) + 1;
+    healthText.innerText = health;
+    monsterHealthText.innerText = monsterHealth;
+    if (health <= 0) {
+        lose();
+      } else if (monsterHealth <= 0) {
+        defeatMonster();
+        if(fighting === 2){
+            winGame()
+          }else{
+            defeatMonster()
+          }
+      }
+}
+function dodge(){
+    text.innerText = "You dodge the attack from the " + monsters[fighting].name
+}
+function defeatMonster(){
+    gold += Math.floor(monsters[fighting].level * 6.7);
+    xp += monsters[fighting].level;
+    goldText.innerText = gold;
+    xpText.innerText = xp;
+    update(locations[4]);
+}
+function lose(){
+    update(locations[5])
+}
+function winGame(){
+    update(locations[6])
+}
+function restart(){
+    xp = 0
+    health = 100
+    gold  = 50
+    currentWeaponIndex = 0
+    inventory = ["stick"];
+    xpText.innerText = xp
+    goldText.innerText = gold
+    healthText.innerText = health
+    goTown()
 }
