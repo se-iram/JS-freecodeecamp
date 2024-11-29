b73c0cd4bd319e93718be6abe1d54edf0d08f898
se-iram <eramhameed5@gmail.com>
2024-11-29 12:29:25 +0500

    javascript

---
+++ 3. Form Vlidation by Building Calorie Counter/script.js
@@ -0,0 +1,13 @@
+const calorieCounter = document.getElementById("calorie-counter")
+const budgetNumberInput = document.getElementById("budget")
+const entryDropdown = document.getElementById("entry-dropdown")
+const addEntryButton = document.getElementById("add-entry")
+const clearButton = document.getElementById("clear")
+const output = document.getElementById("output")
+
+// to check wether user input calue is valid or not
+let isError = false
+// function to clean input string
+function cleanInputString(str){
+    calorieCounter
+}
